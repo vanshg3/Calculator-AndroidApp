dea920d2f98ce38d84d770f16fdd8cbf4f7e97ae
Vansh Gupta <vanshg3@illinois.edu>
2020-11-19 15:07:57 -0800

    final edits

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -14,9 +14,10 @@ public class MainActivity extends AppCompatActivity {
     Button zero, one, two, three, four, five, six,
             seven, eight, nine, addition, subtraction, division,
             multiplication, decimal, clear, equal, sine, cosine, tangent, pi;
+
     EditText text;
 
-    double mValueOne, mValueTwo;
+    double val1, val2;
 
     boolean added, subtracted, multiplied, divided;
 
@@ -26,27 +27,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         zero = findViewById(R.id.button0);
-        one = findViewById(R.id.button1);
-        two = findViewById(R.id.button2);
-        three = findViewById(R.id.button3);
-        four = findViewById(R.id.button4);
-        five = findViewById(R.id.button5);
-        six = findViewById(R.id.button6);
-        seven = findViewById(R.id.button7);
-        eight = findViewById(R.id.button8);
-        nine = findViewById(R.id.button9);
-        pi = findViewById(R.id.button10);
-        addition = findViewById(R.id.buttonadd);
-        subtraction = findViewById(R.id.buttonsub);
-        multiplication = findViewById(R.id.buttonmul);
-        division = findViewById(R.id.buttondiv);
-        clear = findViewById(R.id.buttonC);
-        equal = findViewById(R.id.buttoneql);
-        text = findViewById(R.id.edit);
-        sine = findViewById(R.id.buttonsin);
-        cosine = findViewById(R.id.buttoncos);
-        tangent = findViewById(R.id.buttontan);
-        decimal = findViewById(R.id.buttondecimal);
 
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,12 +35,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        one = findViewById(R.id.button1);
+
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s1", text.getText()));
             }
         });
+
+        two = findViewById(R.id.button2);
 
         two.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,12 +53,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        three = findViewById(R.id.button3);
+
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s3", text.getText()));
             }
         });
+
+        four = findViewById(R.id.button4);
 
         four.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,12 +71,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        five = findViewById(R.id.button5);
+
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s5", text.getText()));
             }
         });
+
+        six = findViewById(R.id.button6);
 
         six.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,12 +89,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        seven = findViewById(R.id.button7);
+
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s7", text.getText()));
             }
         });
+
+        eight = findViewById(R.id.button8);
 
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,12 +107,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        nine = findViewById(R.id.button9);
+
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s9", text.getText()));
             }
         });
+
+        pi = findViewById(R.id.button10);
+
+        pi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(String.format("%s3.1415926535898", text.getText()));
+            }
+        });
+
+        addition = findViewById(R.id.buttonadd);
 
         addition.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,90 +134,47 @@ public class MainActivity extends AppCompatActivity {
                 if (text == null) {
                     text.setText("");
                 } else {
-                    mValueOne = Float.parseFloat(text.getText() + "");
+                    val1 = Float.parseFloat(text.getText() + "");
                     added = true;
                     text.setText(null);
                 }
             }
         });
 
+        subtraction = findViewById(R.id.buttonsub);
+
         subtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
+                val1 = Float.parseFloat(text.getText() + "");
                 subtracted = true;
                 text.setText(null);
             }
         });
 
+        multiplication = findViewById(R.id.buttonmul);
+
         multiplication.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
+                val1 = Float.parseFloat(text.getText() + "");
                 multiplied = true;
                 text.setText(null);
             }
         });
 
+        division = findViewById(R.id.buttondiv);
+
         division.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
+                val1 = Float.parseFloat(text.getText() + "");
                 divided = true;
                 text.setText(null);
             }
         });
 
-        sine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
-                text.setText(ArithmeticLogic.takingSin(mValueOne));
-            }
-        });
-
-        cosine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
-                text.setText(ArithmeticLogic.takingCos(mValueOne));
-            }
-        });
-
-        tangent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueOne = Float.parseFloat(text.getText() + "");
-                text.setText(ArithmeticLogic.takingTan(mValueOne));
-            }
-        });
-
-        equal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mValueTwo = Float.parseFloat(String.format("%s", text.getText()));
-
-                if (added) {
-                    text.setText(ArithmeticLogic.adder(mValueOne, mValueTwo));
-                    added = false;
-                }
-
-                if (subtracted) {
-                    text.setText(ArithmeticLogic.subtracter(mValueOne, mValueTwo));
-                    subtracted = false;
-                }
-
-                if (multiplied) {
-                    text.setText(ArithmeticLogic.multiplier(mValueOne, mValueTwo));
-                    multiplied = false;
-                }
-
-                if (divided) {
-                    text.setText(ArithmeticLogic.divider(mValueOne, mValueTwo));
-                    divided = false;
-                }
-            }
-        });
+        clear = findViewById(R.id.buttonC);
 
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -217,16 +183,73 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        pi.setOnClickListener(new View.OnClickListener() {
+
+        text = findViewById(R.id.edit);
+        sine = findViewById(R.id.buttonsin);
+
+        sine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text.setText(String.format("%s3.14", text.getText()));
+                val1 = Float.parseFloat(text.getText() + "");
+                text.setText(ArithmeticLogic.takingSin(val1));
             }
         });
+
+        cosine = findViewById(R.id.buttoncos);
+
+        cosine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val1 = Float.parseFloat(text.getText() + "");
+                text.setText(ArithmeticLogic.takingCos(val1));
+            }
+        });
+
+        tangent = findViewById(R.id.buttontan);
+
+        tangent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val1 = Float.parseFloat(text.getText() + "");
+                text.setText(ArithmeticLogic.takingTan(val1));
+            }
+        });
+
+        decimal = findViewById(R.id.buttondecimal);
+
         decimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text.setText(String.format("%s.", text.getText()));
+            }
+        });
+
+        equal = findViewById(R.id.buttoneql);
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val2 = Float.parseFloat(String.format("%s", text.getText()));
+
+                if (added) {
+                    text.setText(ArithmeticLogic.adder(val1, val2));
+                    added = false;
+                }
+
+                if (subtracted) {
+                    text.setText(ArithmeticLogic.subtracter(val1, val2));
+                    subtracted = false;
+                }
+
+                if (multiplied) {
+                    text.setText(ArithmeticLogic.multiplier(val1, val2));
+                    multiplied = false;
+                }
+
+                if (divided) {
+                    text.setText(ArithmeticLogic.divider(val1, val2));
+                    divided = false;
+                }
             }
         });
     }
